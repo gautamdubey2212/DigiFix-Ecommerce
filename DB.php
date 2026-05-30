c4c984d515e2db2636afa3ae3a913af2d8e672a4
gautamdubey2212 <gautamdubey221203@gmail.com>
2026-05-30 13:22:02 +0530

    Clear database connection credentials
    Removed sensitive database credentials from DB.php.

--- DB.php
+++ DB.php
@@ -2,10 +2,10 @@
 
 session_start();
 
-$servername = "sql302.infinityfree.com";
-$username = "if0_41982600";
-$password = "Digifix221203";
-$database = "if0_41982600_digifix";
+$servername = "";
+$username = "";
+$password = "";
+$database = "";
 
 $conn = new mysqli($servername,$username,$password,$database);
 
